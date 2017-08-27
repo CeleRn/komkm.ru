68fadd3791f3caedb8a4c83a4fc89ac556fc8676
CeleRn <celern84@gmail.com>
2017-08-27 16:36:30 +0300

    edit webpack config

---
+++ assets/commons.js
@@ -0,0 +1,26 @@
+/////////////////////////////////
+// Загрузка CSS стилей         //
+/////////////////////////////////
+
+require('./styles/main.scss');
+
+/////////////////////////////////
+// Загрузка картинок           //
+/////////////////////////////////
+
+require('./images/logo-komkm.svg');
+require('./images/logo-only-komkm.svg');
+
+/////////////////////////////////
+// Загрузка иконок для спрайта //
+/////////////////////////////////
+
+// main
+require('./icons/home.svg');
+require('./icons/mail.svg');
+require('./icons/phone.svg');
+require('./icons/place.svg');
+require('./icons/search.svg');
+require('./icons/time.svg');
+require('./icons/comment.svg');
+require('./icons/edit.svg');
