3cf3549b6477efd21f5be269b62943e2b96f54c1
CeleRn <celern84@gmail.com>
2017-09-29 03:01:49 +0300

    add icons

--- assets/commons.js
+++ assets/commons.js
@@ -15,45 +15,45 @@ require('./images/logo-only-komkm.svg');
 // Загрузка иконок для спрайта //
 /////////////////////////////////
 
-// // Иконки для интерфейса
-// require('./icons/balance.svg');
-// require('./icons/call.svg');
-// require('./icons/cart.svg');
-// require('./icons/chat.svg');
-// require('./icons/copy.svg');
-// require('./icons/download.svg');
-// require('./icons/favorite.svg');
-// require('./icons/forum.svg');
-// require('./icons/help.svg');
-// require('./icons/home.svg');
-// require('./icons/info.svg');
-// require('./icons/mail.svg');
-// require('./icons/map.svg');
-// require('./icons/person.svg');
-// require('./icons/place.svg');
-// require('./icons/review.svg');
-// require('./icons/repair.svg');
-// require('./icons/search.svg');
-// require('./icons/shipping.svg');
-// require('./icons/time.svg');
-// require('./icons/videocam.svg');
-// require('./icons/warning.svg');
-// // Иконки для оборудования
-// require('./icons/hardware/computer.svg');
-// require('./icons/hardware/gps.svg');
-// require('./icons/hardware/laptop.svg');
-// require('./icons/hardware/mobile.svg');
-// require('./icons/hardware/monoblock.svg');
-// require('./icons/hardware/photocamera.svg');
-// require('./icons/hardware/printer.svg');
-// require('./icons/hardware/tablet.svg');
-// require('./icons/hardware/tv.svg');
-// // Иконки для преимуществ
-// require('./icons/advantages/merge.svg');
-// require('./icons/advantages/khimki.svg');
-// require('./icons/advantages/key.svg');
-// require('./icons/advantages/garanty.svg');
-// require('./icons/advantages/expo.svg');
+// Иконки для интерфейса
+require('./icons/balance.svg');
+require('./icons/call.svg');
+require('./icons/cart.svg');
+require('./icons/chat.svg');
+require('./icons/copy.svg');
+require('./icons/download.svg');
+require('./icons/favorite.svg');
+require('./icons/forum.svg');
+require('./icons/help.svg');
+require('./icons/home.svg');
+require('./icons/info.svg');
+require('./icons/mail.svg');
+require('./icons/map.svg');
+require('./icons/person.svg');
+require('./icons/place.svg');
+require('./icons/review.svg');
+require('./icons/repair.svg');
+require('./icons/search.svg');
+require('./icons/shipping.svg');
+require('./icons/time.svg');
+require('./icons/videocam.svg');
+require('./icons/warning.svg');
+// Иконки для оборудования
+require('./icons/hardware/computer.svg');
+require('./icons/hardware/gps.svg');
+require('./icons/hardware/laptop.svg');
+require('./icons/hardware/mobile.svg');
+require('./icons/hardware/monoblock.svg');
+require('./icons/hardware/photocamera.svg');
+require('./icons/hardware/printer.svg');
+require('./icons/hardware/tablet.svg');
+require('./icons/hardware/tv.svg');
+// Иконки для преимуществ
+require('./icons/advantages/merge.svg');
+require('./icons/advantages/khimki.svg');
+require('./icons/advantages/key.svg');
+require('./icons/advantages/garanty.svg');
+require('./icons/advantages/expo.svg');
 
 /////////////////////////////////
 // Bootstrap 4 JavaScript      //
